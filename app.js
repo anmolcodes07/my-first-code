d3477543b5dae98cc0404c96c0e09587d59f970c
anmolcodes07 <anmol639415@gmail.com>
2026-08-13 00:29:51 +0530

    add new file

---
+++ app.js
@@ -0,0 +1,11 @@
+let array =[1,4,5,5];
+array.unshift(67);
+console.log(array[0]);
+console.log(array);
+array.shift();
+console.log(array);
+let sum = function (a,b){
+  console.log(a+b);
+  console.log(a*b);
+}
+sum(5,6);
